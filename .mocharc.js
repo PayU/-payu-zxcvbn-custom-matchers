8e9d55aab4a571b3dd21e9c8ecbff423d9957306
Ilan Kushnir <ilan@zooz.com>
2024-11-22 13:52:26 +0200

    fix: ci workflow
    chore: trigger
    
    tests: dir hirearchy
    
    tests: provide mocharc
    
    fix: add .test
    
    tests: add types/mocha
    
    tests: add extension ts
    
    tests: downgrade chai to v 4
    
    tests: install nyc
    
    chore: metdata pckage json
    
    chore: nyc config

--- .mocharc.js
+++ .mocharc.js
@@ -4,7 +4,6 @@ module.exports = {
     forbidOnly: !!CI,               // fail the pipeline whenever .only is committed to the tests
     require: [
         'ts-node/register',         // required for running tests with TypeScript
-        'tests/utils/chaiPlugins.ts',      // import chai plugins
     ],
     recursive: true,
     exit: true,
